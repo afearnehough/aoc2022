b5364fa9e85a27bb4bc16acc4a69897e62da92bd
Adam Fearnehough <afearnehough@gmail.com>
2022-12-03 17:19:31 +0000

    Day 3 solved

--- src/day3.rs
+++ src/day3.rs
@@ -11,7 +11,7 @@ pub fn handler(input: &String) -> Result<(), String> {
         }).collect::<Vec<Vec<i32>>>();
 
     println!(
-        "Part A: {:?}",
+        "Part A: {}",
         rucksacks
             .iter()
             .map(|rucksack| {
@@ -28,34 +28,22 @@ pub fn handler(input: &String) -> Result<(), String> {
             .iter()
             .sum::<i32>());
 
-    // let rucksacks = input
-    //         .lines()
-    //         .map(|line| {
-    //             let (first, second) = line.split_at(line.len() / 2);
-    //             let convert = |char: &u8| -> i32 {
-    //                 (if char.is_ascii_uppercase() { char - 38 } else { char - 96 }) as i32
-    //             };
+    println!(
+        "Part B: {:?}",
+        rucksacks
+            .chunks(3)
+            .map(|group| {
+                for item in &group[0] {
+                    if group[1].contains(item) && group[2].contains(item) {
+                        return *item;
+                    }
+                }
 
-    //             let first = first.as_bytes().iter().map(convert).collect::<Vec<i32>>();
-    //             let second = second.as_bytes().iter().map(convert).collect::<Vec<i32>>();
-
-    //             (first, second)
-    //         });
-
-    //     println!(
-    //         "Part A: {}",
-    //         rucksacks
-    //             .map(|(&first, &second)| {
-    //                 let first = first.iter().collect::<HashSet<i32>>();
-    //                 let second = second.iter().collect::<HashSet<i32>>();
-    //                 let intersect = first.intersection(&second).into_iter().collect::<Vec<&i32>>();
-    //                 *intersect[0]
-    //             })
-    //             .collect::<Vec<i32>>()
-    //             .iter()
-    //             .sum::<i32>());
-
-
+                0
+            })
+            .collect::<Vec<i32>>()
+            .iter()
+            .sum::<i32>());
 
     Ok(())
 }
